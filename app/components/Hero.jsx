edfe934c72268b2dcc962b55307988516bce7add
Nikolaj <nikolaj.tetradov@gmail.com>
2025-03-06 23:23:44 +0200

    hero fonts change

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -40,26 +40,21 @@ const Hero = () => {
         {/* Text container with backdrop filter */}
         <motion.div
           variants={fadeIn('down')}
-          className='flex flex-col items-center mx-auto text-black/85 text-left p-7 mt-2 
-    md:mt-20 rounded-lg backdrop-filter backdrop-blur-lg bg-stone-200/40 
-    border border-emerald-500/20 shadow-xl lg:absolute lg:top-[130px] 
-    md:text-left lg:max-w-[600px] xl:max-w-[500px] xl:right-[-28px] 
-    2xl:max-w-[550px] 2xl:right-[16px] 2xl:top-[250px] 3xl:right-[32px]'
+          className='flex flex-col items-center mx-auto text-black/85 text-left p-7 mt-2 md:mt-20 rounded-lg backdrop-filter backdrop-blur-lg bg-stone-200/30 border border-emerald-500/20 shadow-xl lg:absolute lg:top-[130px] md:text-left lg:max-w-[600px] xl:max-w-[500px] xl:right-[-28px] 2xl:max-w-[550px] 2xl:right-[16px] 2xl:top-[250px] 3xl:right-[32px]'
         >
           <motion.h1 variants={fadeIn('down')} className='h1 mb-5 ml-0'>
             {t('title')}
           </motion.h1>
 
-          <motion.p
+          <motion.h4
             variants={fadeIn('left')}
-            className='font-light mb-8 lg:mb-[48px] max-w-lg xl:max-w-xl 
-      2xl:max-w-xl leading-8 xl:text-[20px] flex-wrap tracking-wider'
+            className='h4 font-light mb-8 lg:mb-[48px] max-w-lg xl:max-w-xl 2xl:max-w-xl leading-8 xl:text-[20px] flex-wrap tracking-wider normal-case'
           >
             {t('subtitle')}
             <div className='font-medium mt-4 text-black/90 tracking-wide italic'>
               {t('subtitle2')}
             </div>
-          </motion.p>
+          </motion.h4>
         </motion.div>
 
         {/* sliding text */}
